7d9bb5ee71fdd86880b4f715bac05d78d55823dd
Renato Bessa <bessache@outlook.com>
2020-04-05 09:19:20 -0300

    ajuste e testes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,42 +1,14 @@
 import React, {useState, Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Container,
-  Row,
-  Col,
-  Jumbotron,
-  Button
-} from 'reactstrap';
 
-class App extends Component {
-  constructor(props) {
-      super(props);
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-          isOpen: false
-      };
-  }
-  toggle() {
-      this.setState({
-          isOpen: !this.state.isOpen
-      });
-  }
-  render() {
-      return (
-          <div>
-              <nav class="navbar navbar-default" role="navigation"></nav>
-          </div>
-      );
-  }
+function App (){
+    return (
+        <body>
+            <nav>Trste</nav>
+        </body>
+       
+    )
 }
-
+      
 export default App;
- 
